1f32dea0d0205a5de355d4d241d7c81df8fe5e40
Sunny <sunny.220990@gmail.com>
2013-12-24 14:32:25 +0530

    ace responsive css added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery_nested_form
 //= require_tree .
-
+//= require dataTables/jquery.dataTables
+//= require dataTables/jquery.dataTables.bootstrap
