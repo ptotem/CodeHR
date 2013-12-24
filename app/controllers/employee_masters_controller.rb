9b5f778bd602dd436d6917dba650eb7e53df61a3
Rakesh Rengaraj <rakesh.rengaraj@gmail.com>
2013-12-24 18:27:55 +0530

    ace admin styling done

--- app/controllers/employee_masters_controller.rb
+++ app/controllers/employee_masters_controller.rb
@@ -1,2 +1,12 @@
 class EmployeeMastersController < InheritedResources::Base
+
+  def new
+    @employee_master = EmployeeMaster.new
+    #@rating.write_attribute(:test1, "")
+    @fields = DynamicField.where(:oclass=>"EmployeeMaster")
+    @fields.each do |ss|
+      @employee_master.write_attribute(ss.name.to_sym,"")
+    end
+  end
+
 end
