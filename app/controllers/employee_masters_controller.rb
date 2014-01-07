1bfebc8eb68540e07055647b3829c55b668e7953
Nilesh Panchal <neil08.panchal@gmail.com>
2014-01-07 17:23:26 +0530

    employee master err solved

--- app/controllers/employee_masters_controller.rb
+++ app/controllers/employee_masters_controller.rb
@@ -10,11 +10,20 @@ class EmployeeMastersController < InheritedResources::Base
     end
   end
 
-  #def create
-  #  #render :text => params
-  #  #return
-  #  @employee_master =EmployeeMaster.new(params[:employee_master])
-  #  @employee_master.save
-  #end
+  def create
+    #render :text => params
+    #return
+    @employee_master =EmployeeMaster.new(params[:employee_master])
+    @employee_master.user=User.create!(:email =>@employee_master.official_email, :password =>"password", :password_confirmation =>"password")
+    respond_to do |format|
+      if @employee_master.save
+        format.html { redirect_to @employee_master, notice: 'Company master was successfully created.' }
+        format.json { render json: @employee_master, status: :created, location: @employee_master }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @employee_master.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 end
