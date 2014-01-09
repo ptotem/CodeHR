a25182bb5a6e9ff5cf98029ce2b90033a67b9e19
Sunny <sunny.220990@gmail.com>
2014-01-09 19:44:51 +0530

    Backend for dynamic form generation is done

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -5,6 +5,9 @@ class RatingsController < InheritedResources::Base
     @rating.score_receiveds.build
     #@rating.write_attribute(:test1, "")
     @fields = DynamicField.where(:oclass=>"Rating")
+    @conf=RatingConf.all.asc(:col_sequence)-RatingConf.where(:col_sequence=>nil)
+    #render :json => @conf
+    #return
     @fields.each do |ss|
       @rating.write_attribute(ss.name.to_sym,"")
     end
