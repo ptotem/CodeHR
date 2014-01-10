c807abb2be9854c77f637eeb5718d7b3399fbd29
Sunny <sunny.220990@gmail.com>
2014-01-10 16:18:16 +0530

    client

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,18 @@
 class ClientsController < InheritedResources::Base
+  #def new
+  #  @clie=Client.new
+  #  @clie.admin_users.build
+  #  respond_to do |format|
+  #    format.html # new.html.erb
+  #    format.json { render json: @clie }
+  #  end
+  #end
   def new
-    @client1=Client.new
-    @client1.admin_users.build
+    @client=Client.new
+    @client.admin_users.build
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @client }
+    end
   end
 end
