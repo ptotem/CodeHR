7734b91474d22bd48d99344c1007d90dfccb8569
Nilesh Panchal <neil08.panchal@gmail.com>
2014-01-14 20:03:03 +0530

    form labels trough yml

--- app/controllers/vacancy_masters_controller.rb
+++ app/controllers/vacancy_masters_controller.rb
@@ -1,12 +1,21 @@
 class VacancyMastersController < InheritedResources::Base
 
   def new
+    @form_config= t('config.VacancyMaster.form.new')
+    @form=@form_config[:fields]
+
     @vacancy_master = VacancyMaster.new
     #@rating.write_attribute(:test1, "")
     @fields = DynamicField.where(:oclass=>"VacancyMaster")
     @fields.each do |ss|
       @vacancy_master.write_attribute(ss.name.to_sym,"")
     end
+  end
+
+  def edit
+    @form_config= t('config.VacancyMaster.form.edit')
+    @form=@form_config[:fields]
+    @vacancy_master = VacancyMaster.find(params[:id])
   end
 
 end
