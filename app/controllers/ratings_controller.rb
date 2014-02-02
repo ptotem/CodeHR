06f428c600368859d49116bfaf190a7f19131de5
Nilesh Panchal <neil08.panchal@gmail.com>
2014-02-02 13:45:22 +0530

    changes

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,18 +1,38 @@
 class RatingsController < InheritedResources::Base
 
+  #def new
+  #  @form_config= t('config.Rating.form.new')
+  #  @form=@form_config[:fields]
+  #  @form = @form.sort_by{|_,v| v[:sequence]}
+  #
+  #  @rating = Rating.new
+  #  @rating.score_receiveds.build
+  #  @fields = DynamicField.where(:oclass=>"Rating")
+  #  @conf=RatingConf.all.asc(:col_sequence)-RatingConf.where(:col_sequence=>nil)
+  #  @fields.each do |ss|
+  #    @rating.write_attribute(ss.name.to_sym,"")
+  #  end
+  #end
+
   def new
     @form_config= t('config.Rating.form.new')
     @form=@form_config[:fields]
-    @form = @form.sort_by{|_,v| v[:sequence]}
-
     @rating = Rating.new
-    @rating.score_receiveds.build
+    RatingScale.all.each do |rc|
+      @rating.score_receiveds.build()
+    end
+    #@rating.write_attribute(:test1, "")
     @fields = DynamicField.where(:oclass=>"Rating")
-    @conf=RatingConf.all.asc(:col_sequence)-RatingConf.where(:col_sequence=>nil)
     @fields.each do |ss|
       @rating.write_attribute(ss.name.to_sym,"")
     end
   end
+
+  def create
+    #render :json => params
+    #return
+  end
+
 
   def edit
     @form_config= t('config.Rating.form.edit')
--- app/models/score_received.rb
+++ app/models/score_received.rb
@@ -1,4 +1,5 @@
 class ScoreReceived
   include Mongoid::Document
   field :score, type: Integer
+  field :rating_scale_id, type: String
 end
