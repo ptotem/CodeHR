939e9250d8837ff0d8c01160b6a21f930280ea91
Sunny <sunny.220990@gmail.com>
2014-04-08 11:24:05 +0530

    models are updated

--- app/models/contact_person.rb
+++ app/models/contact_person.rb
@@ -5,6 +5,7 @@ class ContactPerson
   field :designation, type: String
   field :phone1, type: String
   field :phone2, type: String
+  field :email, type: String
   field :deleted, type: Boolean
   embedded_in :vendor_master
 end
--- app/models/job_preference.rb
+++ app/models/job_preference.rb
@@ -3,5 +3,6 @@ class JobPreference
   include Mongoid::Timestamps
   field :vacancy_id, type: String
   field :deleted, type: Boolean
+  field :sequence_no, type: Integer
   embedded_in :candidate_master
 end
