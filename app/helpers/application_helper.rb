641ef8d1352e3a731f9f92afcc27503355839d49
Sunny <sunny.220990@gmail.com>
2014-04-11 12:43:31 +0530

    Bulk update done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -440,9 +440,14 @@ module ApplicationHelper
               (2..spreadsheet.last_row).each do |i|
                 row = Hash[[header, spreadsheet.row(i)].transpose]
                 @cls_name=@pro.step_transacts[0].action_object_id
-                product = eval(@cls_name).new
+                if row["id"]
+                  product = eval(@cls_name).find_by_id(row["id"]) || eval(@cls_name).new
+                else
+                  product = eval(@cls_name).new
+                end
                 product.attributes = row.to_hash
                 product.save!
+
               end
             puts "Bulk create complete."
           #elsif @pro.step_transacts[0].action_name == "Update"
