00a7b17a02bf2112f3cae22e1474de3c85c86399
Nilesh Panchal <nilesh@ptotem.com>
2014-06-04 11:51:40 +0530

    employee master upload bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#User.create!(:email=>"superadmin@codehr.in", :password=>"password", :password_confrimation=>"password", :designation=>"HR")
+#@user = User.create!(:email=>"superadmin@codehr.in", :password=>"password", :password_confrimation=>"password", :designation=>"HR")
+#@user.save!
