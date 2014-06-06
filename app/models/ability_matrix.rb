1f7655391cd3db9f7d3f02784cc8a41482c46710
sunnys <sunny.220990@gmail.com>
2014-06-06 20:37:34 +0530

    Ability bug solved

--- app/models/ability_matrix.rb
+++ app/models/ability_matrix.rb
@@ -2,7 +2,7 @@ class AbilityMatrix
   include Mongoid::Document
   include Mongoid::Timestamps
   field :name, type: String
-  field :role_id, type: String
+  field :role_id, type: Array
   field :access_obj, type: Array
   field :master_pro_id, type: String
   field :deleted, type: Boolean
